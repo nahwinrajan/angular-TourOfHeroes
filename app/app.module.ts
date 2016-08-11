04265907e6d5ff13d0f9550b56147634b324af0e
nahwinrajan <nahwin.rajan@gmail.com>
2016-08-11 18:39:49 +0700

    completed the tutorial.

--- app/app.module.ts
+++ app/app.module.ts
@@ -2,12 +2,19 @@ import { NgModule }       from '@angular/core';
 import { BrowserModule }  from '@angular/platform-browser';
 import { FormsModule }    from '@angular/forms';
 
+/** import for loading and configuring the in-memory web-api */
+import { HttpModule, XHRBackend } from '@angular/http';
+
+import { InMemoryBackendService, SEED_DATA } from 'angular2-in-memory-web-api';
+import { InMemoryDataService }  from './in-memory-data.service';
+
 import { AppComponent } from './app.component';
 import { routing } from './app.routing';
 
 import { HeroesComponent } from './heroes.component';
 import { DashboardComponent } from './dashboard.component';
 import { HeroDetailComponent } from './hero-detail.component';
+import { HeroSearchComponent } from './hero-search.component';
 
 import { HeroService } from './hero.service';
 
@@ -15,16 +22,20 @@ import { HeroService } from './hero.service';
   imports: [
     BrowserModule,
     FormsModule,
-    routing
+    routing,
+    HttpModule
   ],
   declarations: [
     AppComponent,
     HeroesComponent,
     DashboardComponent,
-    HeroDetailComponent
+    HeroDetailComponent,
+    HeroSearchComponent
   ],
   providers: [
-    HeroService
+    HeroService,
+    { provide: XHRBackend, useClass: InMemoryBackendService },
+    { provide: SEED_DATA,  useClass: InMemoryDataService }
   ],
   bootstrap: [ AppComponent ]
 })
